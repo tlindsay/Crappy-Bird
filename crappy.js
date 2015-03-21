eda718ebaa1985d047423f0c1a79fffb0f9d02fd
Patrick Lindsay <tlindsay@una.edu>
2015-03-21 00:41:25 -0500

    Credit where credit is due.

--- crappy.js
+++ crappy.js
@@ -1,3 +1,14 @@
+//   ____                             ____  _         _ 
+//  / ___|_ __ __ _ _ __  _ __  _   _| __ )(_)_ __ __| |
+// | |   | '__/ _` | '_ \| '_ \| | | |  _ \| | '__/ _` |
+// | |___| | | (_| | |_) | |_) | |_| | |_) | | | | (_| |
+//  \____|_|  \__,_| .__/| .__/ \__, |____/|_|_|  \__,_|
+//                 |_|   |_|    |___/                   
+////////////////////////////////////////////////////////////
+// Adapted from Mary Rose Cook's Space Invaders:
+// http://annotated-code.maryrosecook.com/space-invaders/
+// 
+
 ;(function() {
 	var Game = function() {
 		console.info('Welcome to Crappy Bird');
